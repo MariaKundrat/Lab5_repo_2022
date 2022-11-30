9b83df3d1e47efcfb4fbdcc562971c1518c98dab
MariaKundrat <03032005km@gmail.com>
2022-11-30 21:21:44 +0200

    adding Lab5.cpp to repo

---
+++ Lab5.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream> 
+#include <string>
+#include "Methods.h"
+using namespace std;
+#define ARRAY_SIZE 3    
+
+int main() {
+    Shop a;
+    int input;
+    float money;
+    while (1) {
+        cout << "Best 1, Sort 2, Exit 0 \n inp:";
+        cin >> input;
+        switch (input)
+        {
+        case(1):
+            cout << "Enter your budget: ";
+            cin >> money;
+            a.best(money);
+            break;
+        case(2):
+            a.sort();
+            a.outAll();
+            break;
+        default: return 0;
+        }
+    }
+}
